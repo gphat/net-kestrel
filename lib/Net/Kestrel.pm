ea76c11a162bf274dd6acc62aa9f49a4808ae9d8
Cory Watson <gphat@Cory-Watsons-Mac-Pro.local>
2011-06-07 17:44:42 -0500

    Rough skeleton.

--- lib/Net/Kestrel.pm
+++ lib/Net/Kestrel.pm
@@ -1,5 +1,37 @@
-use strict;
-use warnings;
 package Net::Kestrel;
+use Moose;
+
+# ABSTRACT: Kestrel Client for Perl
+
+=head1 SYNOPSIS
+
+    use Net::Kestrel;
+
+=head1 DESCRIPTION
+
+Net::Kestrel is…
+
+=begin :prelude
+
+=head1 NOTES
+
+Notes.
+
+=end :prelude
+
+=cut
+
+1;
+
+__END__
+
+
+=begin :postlude
+
+=head1 CONTRIBUTORS
+
+Me
+
+=end :postlude
 
 1;
